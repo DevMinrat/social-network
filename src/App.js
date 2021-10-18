a2cd78dbfe6e3f3ad551ffde03d6053c67917e98
Marat Minchik <devminrat@gmail.com>
2021-10-18 16:19:28 +0300

    added some blocks, first steps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img
+          src="https://clipart-best.com/img/earth/earth-clip-art-23.png"
+          width="80px"
+        ></img>
       </header>
+      <aside className="aside">
+        <nav>
+          <a href="#">Profile</a>
+          <a href="#">Messages</a>
+          <a href="#">News</a>
+          <a href="#">Music</a>
+          <a href="#">Settings</a>
+        </nav>
+      </aside>
+      <div className="content">
+        <div className="profile">
+          <div>
+            <img
+              className="profile__img"
+              src="https://mebel-go.ru/mebelgoer/88741402242435-oboi-1920h1080.-davaj-ne-videt-melkogo-v-zerkalnom-otrazhenii-20.jpg"
+            ></img>
+          </div>
+          <div>ava+descr</div>
+          <div>
+            All posts
+            <div>New post</div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
