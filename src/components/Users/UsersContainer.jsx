c59e9194e7cba1f24bd0be0e3613e2b72f760801
Marat Minchik <devminrat@gmail.com>
2021-11-19 21:28:32 +0300

    refactored users to class component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,5 +1,9 @@
 import { connect } from "react-redux";
-import { followUserAC, setUsersAC, unfollowUserAC } from "../../redux/usersReducer";
+import {
+  followUserAC,
+  setUsersAC,
+  unfollowUserAC,
+} from "../../redux/usersReducer";
 import Users from "./Users";
 
 const mapStateToProps = (state) => {
