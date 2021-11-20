709416ad449e670ab2bf944665d8028a427bc754
Marat Minchik <devminrat@gmail.com>
2021-11-20 20:34:02 +0300

    deleted old code in users component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,42 +2,6 @@ import * as axios from "axios";
 import React from "react";
 import UserItem from "./UserItem/UserItem";
 import UserAva from "../../assets/images/avatar.gif";
-
-// let Users = (props) => {
-//   let getusers = () => {
-//     if (props.users.length === 0) {
-//       axios
-//         .get("https://social-network.samuraijs.com/api/1.0/users")
-//         .then((response) => {
-//           props.setUsers(response.data.items);
-//         });
-//     }
-//   };
-
-//   let userItems = props.users.map((u) => {
-//     return (
-//       <UserItem
-//         id={u.id}
-//         avatar={u.photos.small ?? UserAva}
-//         name={u.name}
-//         status={u.status}
-//         location={u.location}
-//         followed={u.followed}
-//         followUser={props.followUser}
-//         unfollowUser={props.unfollowUser}
-//         key={u.id}
-//       />
-//     );
-//   });
-
-//   return (
-//     <div>
-//       <button onClick={getusers}>Get Users</button>
-//       {userItems}
-//     </div>
-//   );
-// };
-
 class Users extends React.Component {
   constructor(props) {
     super(props);
