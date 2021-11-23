d35786bdfed52c40976e26a414e3776be6ccfdc4
Marat Minchik <devminrat@gmail.com>
2021-11-23 19:39:18 +0300

    added pagination for users page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,12 +2,16 @@ import * as axios from "axios";
 import React from "react";
 import UserItem from "./UserItem/UserItem";
 import UserAva from "../../assets/images/avatar.gif";
+import style from "./Users.module.css";
 class Users extends React.Component {
   componentDidMount() {
     axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
       .then((response) => {
         this.props.setUsers(response.data.items);
+        this.props.setUsersCount(response.data.totalCount);
       });
   }
 
@@ -29,8 +33,45 @@ class Users extends React.Component {
     });
   };
 
+  onPageChanged = (page) => {
+    this.props.setCurrentPage(page);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+
   render() {
-    return <div>{this.userItems()}</div>;
+    let pagesCount = Math.ceil(
+      this.props.totalUsersCount / this.props.pageSize
+    );
+    const pages = [];
+    {
+      for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+      }
+    }
+
+    return (
+      <div>
+        <div>
+          {pages.map((p) => {
+            return (
+              <span
+                className={this.props.currentPage === p && style.currentPage}
+                onClick={() => this.onPageChanged(p)}
+              >
+                {p}
+              </span>
+            );
+          })}
+        </div>
+        {this.userItems()}
+      </div>
+    );
   }
 }
 
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,14 +1,19 @@
 import { connect } from "react-redux";
 import {
   followUserAC,
+  setCurrentPageAC,
   setUsersAC,
   unfollowUserAC,
+  setTotalUsersCountAC,
 } from "../../redux/usersReducer";
 import Users from "./Users";
 
 const mapStateToProps = (state) => {
   return {
     users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
   };
 };
 
@@ -22,6 +27,12 @@ const mapDispatchToProps = (dispatch) => {
     },
     setUsers: (users) => {
       dispatch(setUsersAC(users));
+    },
+    setCurrentPage: (currentPage) => {
+      dispatch(setCurrentPageAC(currentPage));
+    },
+    setUsersCount: (usersTotalCount) => {
+      dispatch(setTotalUsersCountAC(usersTotalCount));
     },
   };
 };
